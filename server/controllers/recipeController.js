1eae38c5525f93f7693f9753186d276fa8647a0e
Malcom Nyaranga <kibisu14@icloud.com>
2024-09-13 00:14:05 +0300

    added image on submit

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -147,13 +147,32 @@ exports.submitRecipeOnPost = async (req, res) => {
 
   try {
 
+    let imageUploadFile;
+    let uploadPath;
+    let newImageName;
+
+     if(!req.files || Object.keys(req.files).length === 0){
+      console.log('No Files where uploaded.');
+    } else {
+
+      imageUploadFile = req.files.image;
+      newImageName = Date.now() + imageUploadFile.name;
+
+      uploadPath = require('path').resolve('./') + '/public/uploads/' + newImageName;
+
+      imageUploadFile.mv(uploadPath, function(err){
+        if(err) return res.satus(500).send(err);
+      })
+
+    } // end of if statement
+
     const newRecipe = new Recipe({
       name: req.body.name,
       description: req.body.description,
       email: req.body.email,
       ingredients: req.body.ingredients,
       category: req.body.category,
-      image: 'Githeri.jpeg'
+      image: newImageName
     });
 
     await newRecipe.save();
