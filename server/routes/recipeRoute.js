eee9cbfbbb91b031c6bb9f88037cca4bd61e58da
teddy papa <papateddyetyang@gmail.com>
2025-01-03 16:18:06 +0300

    project edning

---
+++ server/routes/recipeRoute.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const recipeController = require('../controllers/recipeController');
+
+/**
+ * App Routes 
+*/
+router.get('/', recipeController.homepage);
+router.get('/recipe/:id', recipeController.exploreRecipe );
+router.get('/categories', recipeController.exploreCategories);
+router.get('/categories/:id', recipeController.exploreCategoriesById);
+router.post('/search', recipeController.searchRecipe);
+router.get('/explore-latest', recipeController.exploreLatest);
+router.get('/explore-random', recipeController.exploreRandom);
+router.get('/submit-recipe', recipeController.submitRecipe);
+router.post('/submit-recipe', recipeController.submitRecipeOnPost);
+router.get('/recipe/:id/edit', recipeController.editRecipe);
+router.post('/recipe/:id/edit', recipeController.editRecipeOnPost);
+router.get('/recipe/:id/delete', recipeController.deleteRecipe);
+
+
+console.log(recipeController); // Check what is being imported
+
+module.exports = router;
